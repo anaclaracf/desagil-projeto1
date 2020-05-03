42d2b3d28625cd68e2c00cb927ec064bf05103a2
Ana Clara Carneiro de Freitas <anaclara.2001@hotmail.com>
2020-05-03 18:07:50 -0300

    'enviar'

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Historia3.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Historia3.java
@@ -1,9 +1,13 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +21,7 @@ import android.widget.Toast;
 public class Historia3 extends AppCompatActivity {
 
     private static final int PICK_CONTACT = 0;
+    private static final int REQUEST_READ_CONTACTS = 0;
 
     private void showToast(String text) {
         Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
@@ -24,8 +29,7 @@ public class Historia3 extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed()
-    {
+    public void onBackPressed() {
         startActivity(new Intent(Historia3.this, Historia1.class));
         onPause();
     }
@@ -34,7 +38,7 @@ public class Historia3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historia3);
-        TextView answer = (TextView)findViewById(R.id.text_message);
+        TextView answer = (TextView) findViewById(R.id.text_message);
         String valor = getIntent().getStringExtra("blabla");
         answer.setText(valor);
 
@@ -43,8 +47,15 @@ public class Historia3 extends AppCompatActivity {
         Button buttonContacts = findViewById(R.id.button_contacts);
 
         buttonContacts.setOnClickListener((view) -> {
-            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-            startActivityForResult(intent, PICK_CONTACT);
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, PICK_CONTACT);
+            } else {
+                String[] permissions = new String[]{
+                        Manifest.permission.READ_CONTACTS,
+                };
+                ActivityCompat.requestPermissions(this, permissions, REQUEST_READ_CONTACTS);
+            }
         });
 
         buttonSend.setOnClickListener((view) -> {
@@ -56,7 +67,7 @@ public class Historia3 extends AppCompatActivity {
             if (phone.isEmpty()) {
                 showToast("Nenhum contato foi selecionado!");
                 return;
-            }else{
+            } else {
                 showToast("Mensagem enviada com sucesso!");
                 answer.setText("");
                 textPhone.setText("");
@@ -73,34 +84,27 @@ public class Historia3 extends AppCompatActivity {
         TextView textPhone = findViewById(R.id.text_phone);
 
         switch (reqCode) {
-            case (PICK_CONTACT) :
+            case (PICK_CONTACT):
                 if (resultCode == Activity.RESULT_OK) {
                     Uri contactData = data.getData();
-                    Cursor c =  getContentResolver().query(contactData, null, null, null, null);
-                    Cursor phones = getContentResolver().query(Phone.CONTENT_URI, null);
+                    Cursor c = getContentResolver().query(contactData, null, null, null, null);
                     if (c.moveToFirst()) {
-                        String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                        String number = c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-//                        System.out.println(number);
-                        textPhone.setText(name);
+                        String contactId = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+                        String hasNumber = c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                        String num = "";
+                        if (Integer.valueOf(hasNumber) == 1) {
+                            Cursor numbers = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+                            while (numbers.moveToNext()) {
+                                num = numbers.getString(numbers.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                textPhone.setText(num);
+                                //Toast.makeText(Historia3.this, "Number=" + num, Toast.LENGTH_LONG).show();
+                            }
+
+                        }
                     }
+                    break;
                 }
-                break;
         }
+
     }
-
-    String phone = textPhone.getText().toString();
-// Esta verificação do número de telefone é bem
-// rígida, pois exige até mesmo o código do país.
-if (!PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
-        showToast("Número inválido!");
-        return;
-    }
-    // Enviar uma mensagem de SMS. Por simplicidade,
-// não estou verificando se foi mesmo enviada,
-// mas é possível fazer uma versão que verifica.
-    SmsManager manager = SmsManager.getDefault();
-        manager.sendTextMessage(phone, null, message, null, null);
-
-
 }
