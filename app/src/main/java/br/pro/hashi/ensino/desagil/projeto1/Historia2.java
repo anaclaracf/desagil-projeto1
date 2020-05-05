5b3efd8229a5b96a373ac6f2d26d4ed9c49d07f5
Joao Almeida <joaoalmeida@Joaos-MacBook-Pro-2.local>
2020-05-05 20:46:31 -0300

    Historia 2 finalizada

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Historia2.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Historia2.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 public class Historia2 extends AppCompatActivity {
 
@@ -15,30 +17,46 @@ public class Historia2 extends AppCompatActivity {
         setContentView(R.layout.activity_historia2);
 
         TextView textMorse = findViewById(R.id.text_morse);
+        TextView texttraduc = findViewById(R.id.text_traduc);
         Button buttonwrite = findViewById(R.id.button_write);
         Button buttonbar = findViewById(R.id.button_bar);
         Button buttonspace = findViewById(R.id.button_space);
-
-
+        Button buttondel = findViewById(R.id.button_delete);
+        Translator tradutor = new Translator();
+        StringBuilder sb = new StringBuilder();
+//        sb.append(a);
+//        sb.append(b);
+//        sb.append(c);
+//        str = sb.toString();
 
         buttonwrite.setOnClickListener((view) -> {
-            Intent intent = new Intent(MainActivity.this, Historia2.class);
-            startActivity(intent);
-            onPause();
+            textMorse.append(".");
         });
-//
-//        buttonbar.setOnClickListener((view) -> {
-//            Intent intent = new Intent(MainActivity.this, Historia2.class);
-//            startActivity(intent);
-//            onPause();
-//        });
-//        buttonspace.setOnClickListener((view) -> {
-//            Intent intent = new Intent(MainActivity.this, Historia2.class);
-//            startActivity(intent);
-//            onPause();
-//        });
+        buttonwrite.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                // Do what you want to do atfer a long click here
+                textMorse.append("-");
+                return true;
+            }
+        });
+
+        buttonspace.setOnClickListener((view) -> {
+            sb.append(tradutor.morseToChar(textMorse.getText().toString()));
+            texttraduc.setText(sb.toString());
+            textMorse.setText("");
+        });
+
+        buttonbar.setOnClickListener((view) -> {
+            sb.append(" ");
+            texttraduc.setText(sb.toString());
+        });
+
+        buttondel.setOnClickListener((view) -> {
+            if (textMorse.getText().toString() != "") {
+                textMorse.setText(textMorse.getText().toString().substring(0, textMorse.length() - 1));
+            }
+        });
     }
-
-
 
 }
